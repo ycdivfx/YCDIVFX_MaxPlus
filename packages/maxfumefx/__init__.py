5bc83ff26f1678dd05913dd8c808b63269adbb5e
Artur Leao <artur.leao@gmail.com>
2013-10-22 22:55:40 +0000

    Fixing class comparision

--- packages/maxfumefx/__init__.py
+++ packages/maxfumefx/__init__.py
@@ -1,13 +1,12 @@
 import MaxPlus
 
 
-def getfumefxgrids(prefix='fumefx'):
+def getfumefxgrids():
     """Helper function that returns a list with all FumeFx Grids in the scene
-    Note: I still need to figure out if we can match the object class against the fumefx class, which would be better.
     """
     nodes = []
     for node in MaxPlus.Core.GetRootNode().Children:
-        if 'fumefx' in prefix:
+        if 'fumefx' in node.Object.GetClassName().lower():
             nodes.append(node)
 
     return nodes
