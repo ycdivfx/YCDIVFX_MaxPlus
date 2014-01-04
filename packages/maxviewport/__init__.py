1bc1c9c561a7d40e181280ccf4393e92d84ccbd0
Artur Leao <artur.leao@gmail.com>
2014-01-04 08:54:12 +0000

    Updating viewport grabbing package and example

--- packages/maxviewport/__init__.py
+++ packages/maxviewport/__init__.py
@@ -1,8 +1,5 @@
-try:
-    import MaxPlus
-except ImportError:
-    MaxPlus = None
-    print 'MaxPlus not present'
+import MaxPlus
+from maxhelpers import mxs_eval, BitmapTypes, ImageFileType
 
 
 class VpGrabType():
@@ -14,24 +11,60 @@ class VpGrabType():
     viewport = "viewport"
 
 
-
 class VpGrab():
     def __init__(self, method=VpGrabType.gw):
         """This class provides you methods to grab the 3dsmax viewport"""
         self.method = method
-        pass
+
+    def ActiveViewport(self, name=(MaxPlus.PathManager.GetRenderOutputDir()
+                                   + r'\default'), FileType=ImageFileType.JPG):
+        """Grabs viewport to a file on the hard-drive.
+
+        :param str name: a valid path to an image file, no extension required
+        :param ImageFileType FileType: one of the available types in ImageFileType
+
+        :rtype:  MaxPlus.Bitmap
+        """
+        # Create storage
+        storage = MaxPlus.Factory.CreateStorage(BitmapTypes.BMM_LINE_ART)
+
+        # Create BitmapInfo
+        bmi = storage.GetBitmapInfo()
+        # Set filename
+        bmi.SetName(name + FileType)
+
+        print bmi.GetName()
+
+        # Create bitmap to hold the dib
+        bmp = MaxPlus.Factory.CreateBitmap()
+
+        # Viewport Manager
+        vm = MaxPlus.ViewportManager
+        # Get active viewport
+        av = vm.GetActiveViewport()
+        # Grab the viewport dib into the bitmap
+        av.GetDIB(bmi, bmp)
+
+        # Open bitmap for writing
+        bmp.OpenOutput(bmi)
+        # Save to file
+        bmp.Write(bmi)
+        # Close bitmap
+        bmp.Close(bmi)
+
+        return bmp
 
     def tofile(self, filename):
         """Grabs viewport to a file on the hard-drive."""
-        MaxPlus.Core_EvalMAXScript('''grab = ''' + self.method + '''.GetViewportDIB()
+        args = '''grab = ''' + self.method + '''.GetViewportDIB()
         grab.filename = @"''' + filename + '''"
         save grab
-        close grab''')
-        return True
+        close grab'''
+        mxs_eval(args)
 
     def toclipboard(self):
         """Grabs viewport to the clipboard."""
-        MaxPlus.Core_EvalMAXScript('''grab = ''' + self.method + '''.GetViewportDIB()
+        args = '''grab = ''' + self.method + '''.GetViewportDIB()
         setclipboardbitmap grab
-        close grab''')
-        return True
+        close grab'''
+        mxs_eval(args)
