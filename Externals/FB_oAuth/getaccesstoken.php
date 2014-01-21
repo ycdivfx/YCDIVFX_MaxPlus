f65e308c3513d0800e2e703c6569bebc7efaf001
Artur Leao <artur.leao@gmail.com>
2014-01-21 21:59:26 +0000

    Adding FB oAuth PHP code.

---
+++ Externals/FB_oAuth/getaccesstoken.php
@@ -0,0 +1,20 @@
+<?php
+require_once("facebook.php");
+
+//instantiate the Facebook library with the APP ID and APP SECRET
+$facebook = new Facebook(array(
+	'appId'  => 'yourAppID',
+	'secret' => 'yourAppSecret',
+	'fileUpload' => false, // optional
+	'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
+));
+
+$user = $facebook->getUser();
+
+if ($user) {
+	$user_profile = $facebook->api('/me');
+	$access_token = $facebook->getAccessToken();
+    // Pass over the access token back to our python webserver to be caught by the BaseHTTPRequestHandler and stored
+	header("Location: http://127.0.0.1?access_token=". $access_token);
+}
+?>
---
+++ Externals/FB_oAuth/index.php
@@ -0,0 +1,32 @@
+<?php
+require_once("facebook.php");
+
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+	'appId'  => 'yourAppID',
+	'secret' => 'yourAppSecret',
+	'fileUpload' => false, // optional
+	'allowSignedRequest' => false, // optional, but should be set to false for non-canvas apps
+));
+
+$post_login = 'http://www.yourserver.com/getaccesstoken.php';
+
+// Get User ID
+$user = $facebook->getUser();
+
+// If the user is logged in and with permissions, let's move on and get the access token.
+if ($user) 
+{	
+	header("Location: ". $post_login);
+} 
+else 
+{
+	$params = array(
+		redirect_uri => $post_login,
+		scope => 'read_stream,publish_stream'
+	);
+  $loginUrl = $facebook->getLoginUrl($params);
+  
+  header("Location: ". $loginUrl);
+}
+?>
