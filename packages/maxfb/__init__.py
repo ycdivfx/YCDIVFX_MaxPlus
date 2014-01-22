15984fc45fa5e185877d2b49b6062c84dc7b26e2
Artur Leao <artur.leao@gmail.com>
2014-01-22 00:32:14 +0000

    Adding docstrings

--- packages/maxfb/__init__.py
+++ packages/maxfb/__init__.py
@@ -7,20 +7,30 @@ class FbConnector():
         self.token = fbauth.getAccessToken()
         self.graph = GraphAPI(self.token)
 
-    def postimage(self, caption, filename):
+    def postImage(self, caption, filename):
+        """Posts an image file into FB
+        :param str caption: a valid string
+        :param str filename: a valid path to an image file
+        """
         try:
             fimage = open(filename, 'rb')
             self.graph.post(path="me/photos", caption=caption, source=fimage)
         except:
             print 'Error submitting image'
 
-    def postmessage(self, text):
+    def postMessage(self, text):
+        """Posts a message into FB
+        :param str text: a valid string
+        """
         try:
             self.graph.post(path='me/feed', message=text)
         except:
             print 'There was an error posting the message'
 
-    def getusername(self):
+    def getUsername(self):
+        """Gets the current logged in username
+        :rtype:  str
+        """
         try:
             r = self.graph.get('me?fields=name')
             return r['name']
