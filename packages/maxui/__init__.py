f125a7306eed9c4f143efd804f15b45880f65624
Daniel Santana <danielgsantana@gmail.com>
2014-05-22 13:08:05 +0100

    Added MaxWindow decorator with support for pareting (by default is on).

---
+++ packages/maxui/__init__.py
@@ -0,0 +1,74 @@
+import MaxPlus
+from PySide import QtGui, QtCore
+
+
+class _GCProtector(object):
+    """
+    Garbage protector for QWidget classes and subclasses.
+    """
+    widgets = []
+
+
+def MaxWindow(widget, parented=True):
+    """
+    Decorator for QWidget, making it work with 3dsmax.
+    @param widget - Widget to be handled
+    @param parented - Should we parent to the 3dsmax MaxWindow.
+    @return - The decorated class
+    """
+    orig_init = widget.__init__
+    orig_show = widget.show
+
+    def __init__(self, *args, **kwargs):
+        defs = {'stylename': 'Plastique', 'theme': 'light'}
+        orig_init(self, *args, **kwargs)
+        defs.update(kwargs)
+        # Make widget garbage collected.
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self._parented = parented
+        self._has_parent = False
+        self._setupUI(defs['stylename'], defs['theme'])
+
+    def _setupUI(self, stylename, theme):
+        for control in self.children():
+            if hasattr(control, 'setStyle'):
+                control.setStyle(QtGui.QStyleFactory.create(stylename))
+
+        # Dark theme detection.
+        if MaxPlus.Core.EvalMAXScript('((colorman.getcolor #window) * 255)[1] < 120'
+                                      ' and ((colorman.getcolor #window) * 255)[2] < 120'
+                                      ' and ((colorman.getcolor #window) * 255)[3] < 120'):
+            theme = 'dark'
+
+        if theme.lower() == 'dark':
+            self.setStyleSheet('QWidget {'
+                               'background: #444444;'
+                               'color: #d8d8d8;'
+                               'selection-color: black;'
+                               'selection-background-color: #3399ff;'
+                               '}')
+        else:
+            self.setStyleSheet('QWidget {'
+                               'background: #E6E6E6;'
+                               'color: #000000;'
+                               'selection-color: black;'
+                               'selection-background-color: #3399ff;'
+                               '}')
+
+    def show(self):
+        if self not in _GCProtector.widgets:
+            _GCProtector.widgets.append(self)
+        orig_show(self)
+        if self._parented and not self._has_parent:
+            import ctypes
+            capsule = self.effectiveWinId()
+            ctypes.pythonapi.PyCObject_AsVoidPtr.restype = ctypes.c_void_p
+            ctypes.pythonapi.PyCObject_AsVoidPtr.argtypes = [ctypes.py_object]
+            ptr = ctypes.pythonapi.PyCObject_AsVoidPtr(capsule)
+            MaxPlus.Win32.Set3dsMaxAsParentWindow(ptr)
+            self._has_parent = True
+
+    widget._setupUI = _setupUI
+    widget.show = show
+    widget.__init__ = __init__
+    return widget
